e07b32fcd0ff58c22454a07684519b8f7aea8fb3
Ajarek <ajarek@poczta.onet.pl>
2024-02-10 23:45:35 +0100

    added Thumbnails

--- app/page.js
+++ app/page.js
@@ -2,11 +2,8 @@ import PopularBikes from '@/components/PopularBikes'
 
 const Home = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-between px-12 py-4">
-      <h1 className='text-center text-2xl'>Popular bikes</h1>
-       <p className='text-center'>
-        The Worlds Premium Brands In One Destination
-       </p>
+    <div className="flex min-h-screen flex-col items-center justify-start px-12 py-4">
+     
       <PopularBikes/>
     </div>
   )
--- components/Carousel.jsx
+++ components/Carousel.jsx
@@ -1,38 +1,85 @@
 'use client'
 
+import { useState } from 'react'
 import Image from 'next/image'
 import { urlFor } from '@/lib/sanity'
 import { Swiper, SwiperSlide } from 'swiper/react'
-import { Navigation, Pagination } from 'swiper/modules'
+import { FreeMode, Navigation, Thumbs } from 'swiper/modules'
 
 import { images } from '@/lib/images'
 
 import 'swiper/css'
+import 'swiper/css/free-mode'
 import 'swiper/css/navigation'
-import 'swiper/css/pagination'
+import 'swiper/css/thumbs'
 
-export default function Carusel({bikes}) {
+export default function Carousel({bikes}) {
+  const [thumbsSwiper, setThumbsSwiper] = useState(null)
+
   return (
-    <section className=' mx-auto flex items-center justify-center  p-4'>
+    <section className=''>
       <div className='container'>
+        <div className='flex  items-center justify-center'>
+          <div>
+
+        <h1 className='text-left text-2xl uppercase font-bold mb-2'>Where <span className='text-red-500'>Joyful</span> <br/> Cycling Begins</h1>
+       <p className='text-left'>
+        The Worlds Premium Brands <br/> In One Destination
+       </p>
+       <div>
+       
+       </div>
+          </div>
+          
         <Swiper
-          navigation
-          pagination={{ type: 'fraction' }}
-          modules={[Navigation, Pagination]}
-          onSwiper={swiper => console.log(swiper)}
-          className='h-[220px] w-[460px] rounded-lg  '
+          loop={true}
+          spaceBetween={10}
+          navigation={false}
+          thumbs={{
+            swiper:
+              thumbsSwiper && !thumbsSwiper.destroyed ? thumbsSwiper : null
+          }}
+          modules={[FreeMode, Navigation, Thumbs]}
+          className='h-[246] w-[400px] rounded-lg'
         >
           {bikes.map((image, index) => (
             <SwiperSlide key={index}>
-              <div className='flex h-full w-full items-center justify-center  px-12 border-2 '>
+              <div className='flex h-full w-full items-center justify-center'>
                 <Image
                   src={urlFor(image.images[0]).url()}
                   alt={image.name}
-                  width={460}
-                  height={220}
+                  width={400}
+                  height={256}
                   className='block h-full w-full object-cover'
                 />
               </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+        </div>
+
+        {/* Thumbnail */}
+        <Swiper
+          onSwiper={setThumbsSwiper}
+          loop={true}
+          spaceBetween={12}
+          slidesPerView={4}
+          freeMode={true}
+          watchSlidesProgress={true}
+          modules={[FreeMode, Navigation, Thumbs]}
+          className='thumbs mt-3 h-[150px] w-[900px] rounded-lg bg-slate-200 '
+        >
+          {bikes.map((image, index) => (
+            <SwiperSlide key={index}>
+              <button className='flex h-full w-full items-center justify-center border-2  p-5'>
+                <Image
+                  src={urlFor(image.images[0]).url()}
+                  alt={image.name}
+                  width={200}
+                  height={123}
+                  className='block h-full w-full object-cover'
+                />
+              </button>
             </SwiperSlide>
           ))}
         </Swiper>
